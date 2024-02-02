e925bba593bc0d1897c001b2d8fe393b8642d6c2
Jean IBARZ <ibarz.jean@gmail.com>
2024-02-02 23:45:16 +0100

    Update devcontainer, VS code settings, requirements, and gitignore.

--- .devcontainer/post-create-wrapper.bash
+++ .devcontainer/post-create-wrapper.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Navigate to the workspace directory where requirements.txt is located
+cd /workspace
+
+pip install -r requirements.txt
+
 echo "Dev Container successfully set up !"
