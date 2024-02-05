fbff326cb9cd53b40ff32ff5e5f0337651f389e7
Jean IBARZ <ibarz.jean@gmail.com>
2024-02-05 23:08:02 +0100

    Update of devcontainer related files to use vscode user instead of root, and use virtual environment with correct interpreter. Fixed launches configuration for finetuning, and updated .gitignore file.

--- .devcontainer/post-create-wrapper.bash
+++ .devcontainer/post-create-wrapper.bash
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# Navigate to the workspace directory where requirements.txt is located
+# Navigate to the workspace directory
 cd /workspace
 
+# Create a virtual environment
+python3 -m venv .venv
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install llama-cpp-python (build with cuda)
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir
+
+# Install dependencies
 pip install -r requirements.txt
+pip install pytest cmake scikit-build setuptools fastapi uvicorn sse-starlette pydantic-settings starlette-context guidance
 
 echo "Dev Container successfully set up !"
